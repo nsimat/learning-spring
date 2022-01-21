f33f2e0e91f5b47379249f1c415528395156e60f
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2022-01-21 18:28:16 +0100

    Initial commit. Creating project

---
+++ src/test/java/edu/nsimat/lil/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nsimat.lil.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
