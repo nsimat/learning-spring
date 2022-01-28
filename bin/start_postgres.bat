6cbf818d478fc77d3e6e4167d2d0f72587195f7e
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2022-01-28 14:21:55 +0100

    Adding Room and Guest entities.

---
+++ bin/start_postgres.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set -euo pipefail
+which psql > /dev/null || (echoerr "Please ensure that postgres client is in your PATH" && exit 1)
+
+mkdir -p %HOME%\docker\volumes\postgres
+del /s %HOME%\docker\volumes\postgres\data
+
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=P@$$w0rd! -e POSTGRES_DB=dev -d -p 5432:5432 -v %HOME%\docker\volumes\postgres:\var\lib\postgresql postgres
+sleep 3
+export PGPASSWORD=P@$$w0rd!
+psql -U postgres -d dev -h localhost -f schema.sql
+psql -U postgres -d dev -h localhost -f data.sql
