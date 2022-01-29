1e8d006ae6fe22301d07604aa1d6ee575e012fa4
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2022-01-29 20:45:07 +0100

    Adding GuestController and hotel-guests view

---
+++ src/main/java/edu/nsimat/lil/learningspring/web/GuestController.java
@@ -0,0 +1,23 @@
+package edu.nsimat.lil.learningspring.web;
+
+import edu.nsimat.lil.learningspring.business.ReservationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/guests")
+public class GuestController {
+    private final ReservationService reservationService;
+
+    public GuestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getGuests(Model model){
+        model.addAttribute("guests", reservationService.getHotelGuests());
+        return "hotel-guests";
+    }
+}
